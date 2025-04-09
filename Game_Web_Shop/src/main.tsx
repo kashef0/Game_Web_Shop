9a05a9e8d98214f7d7eb740b154c4b2af7bffd49
M.kashef <kashef.sy@gmail.com>
2025-04-09 15:13:05 +0200

    satte ig[ng projekt, installerade redux tool kit, chakra ui, react router och skapade routing fil samt justerade main.ts

--- Game_Web_Shop/src/main.tsx
+++ Game_Web_Shop/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { Provider as ChakraProvider } from "@/components/ui/provider"
+import { RouterProvider } from 'react-router-dom'
+import router from './routing'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <ChakraProvider>
+        <RouterProvider router={router}  />
+      </ChakraProvider>
+    </Provider>
+  </StrictMode>
 )
