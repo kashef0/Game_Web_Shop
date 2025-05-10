e5a6ba3725a068d44a2f451c59bd91facf62e062
M.kashef <kashef.sy@gmail.com>
2025-05-10 16:06:19 +0200

    justerade spel interfaces

--- Game_Web_Shop/src/types/Game.ts
+++ Game_Web_Shop/src/types/Game.ts
@@ -1,19 +1,33 @@
-
+// games interfaces
 export interface Platform {
     id: number,
     name: string,
     slug: string
 }
 
+
 export interface Games {
     id: number,
     name: string,
     background_image: string,
     parent_platforms: { platform: Platform }[],
-    metacritic: number
+    metacritic: number;
+    rating_top: number;
+    rating: number,
+    genres: Genre[]
+}
+
+export interface Genre {
+    id: number,
+    name:string,
+    image_background: string
 }
 
 
+export interface FetchGenresRes {
+    count: number,
+    results: Genre[]
+    }
 
 export interface FetchGameRes {
     count: number;
@@ -22,6 +36,62 @@ export interface FetchGameRes {
 
 
 
+  
+  export interface PlatformWrapper {
+    id: number,
+    name: string,
+    slug: string,
+    platforms: Platform[]
+  }
+  
+  export interface PlatFormResults {
+    results: PlatformWrapper[]
+  }
 
 
+export interface GameDetails {
+    id: number,
+    name: string,
+    description: string,
+    metacritic: number,
+    released: Date,
+    background_image: string,
+    website: string,
+    rating: number,
+    playtime: number,
+    platforms: {
+      platform: {
+        name: string;
+      };
+    }[];
+}
+
+
+export interface GameTrailer {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: TrailerResult[];
+}
+
+export interface TrailerResult {
+  id: number;
+  name: string;
+  preview: string;
+  data: {
+    480: string;
+    max: string;
+  };
+}
+
+
+export interface BackendGameData {
+  _id: string;
+  rawgId: number;
+  price: number;
+  rentalPrice?: number;
+  availableForRent: boolean;
+}
+
+export type FullGame = Games & BackendGameData;
 
